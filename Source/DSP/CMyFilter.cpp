d68ca0b94594bb164265ce7f78b9e6bd5fa89adc
Tatsuya Shiozawa <shio.tatsu99@gmail.com>
2016-03-27 20:57:30 +0900

    FirstCommit

---
+++ Source/DSP/CMyFilter.cpp
@@ -0,0 +1,226 @@
+#pragma once;
+#include <math.h>
+#include <algorithm>
+
+
+// --------------------------------------------------------------------------------
+// CMyFilter
+// --------------------------------------------------------------------------------
+class CMyFilter
+{
+private:
+	float a0, a1, a2, b0, b1, b2;
+
+	float out1, out2;
+	float in1, in2;
+
+public:
+	inline CMyFilter();
+
+	inline float Process(float in);
+	inline void ClockProcess(float* inL, float* inR);
+	inline void SetFilter(int mode, float freq, float bw, float gain = 1.0f, float samplerate = 44100.0f);
+
+	inline void LowPass(float freq, float q, float samplerate = 44100.0f);
+	inline void HighPass(float freq, float q, float samplerate = 44100.0f);
+	inline void BandPass(float freq, float bw, float samplerate = 44100.0f);
+	inline void Notch(float freq, float bw, float samplerate = 44100.0f);
+	inline void LowShelf(float freq, float q, float gain, float samplerate = 44100.0f);
+	inline void HighShelf(float freq, float q, float gain, float samplerate = 44100.0f);
+	inline void Peaking(float freq, float bw, float gain, float samplerate = 44100.0f);
+	inline void AllPass(float freq, float q, float samplerate = 44100.0f);
+	
+	enum FilterType { LP = 0, HP, BP, N, LoSh, HiSh, Peak, AP};
+};
+
+// --------------------------------------------------------------------------------
+//
+// --------------------------------------------------------------------------------
+CMyFilter::CMyFilter()
+{
+
+	a0 = 1.0f;
+	a1 = 0.0f;
+	a2 = 0.0f;
+	b0 = 1.0f;
+	b1 = 0.0f;
+	b2 = 0.0f;
+
+	in1 = 0.0f;
+	in2 = 0.0f;
+
+	out1 = 0.0f;
+	out2 = 0.0f;
+}
+
+
+
+// --------------------------------------------------------------------------------
+// 
+// --------------------------------------------------------------------------------
+float CMyFilter::Process(float in)
+{
+	float out = b0 / a0 * in + b1 / a0 * in1 + b2 / a0 * in2
+		- a1 / a0 * out1 - a2 / a0 * out2;
+
+	in2 = in1;
+	in1 = in;
+
+	out2 = out1;
+	out1 = out;
+
+	return out;
+}
+
+void CMyFilter::ClockProcess(float* LeftSample, float* RightSample)
+{
+	*LeftSample = Process(*LeftSample);
+	*RightSample = Process(*RightSample);
+}
+
+void CMyFilter::SetFilter(int mode, float freq, float bw, float gain, float samplerate)
+{
+	//Parameters Range Set. mininume -> maximum.
+	//freq = freq * 1920.0f + 80.0f; //max:2khz, min:80hz
+	//bw = bw * 23.0f + 1.0f;
+
+    switch (mode)
+	{
+	case 0:
+		LowPass(freq, bw);
+		break;
+	case 1:
+		HighPass(freq, bw);
+		break;
+	case 2:
+		BandPass(freq, bw);
+		break;
+	case 3:
+		Notch(freq, bw);
+		break;
+	case 4:
+		LowShelf(freq, bw, gain);
+		break;
+	case 5:
+		HighShelf(freq, bw, gain);
+		break;
+	case 6:
+		Peaking(freq, bw, gain);
+		break;
+	case 7:	
+		AllPass(freq, bw);
+		break;
+	}
+}
+// --------------------------------------------------------------------------------
+// 
+// --------------------------------------------------------------------------------
+void CMyFilter::LowPass(float freq, float q, float samplerate)
+{
+	float omega = 2.0f * 3.14159265f *  freq / samplerate;
+	float alpha = sin(omega) / (2.0f * q);
+
+	a0 = 1.0f + alpha;
+	a1 = -2.0f * cos(omega);
+	a2 = 1.0f - alpha;
+	b0 = (1.0f - cos(omega)) / 2.0f;
+	b1 = 1.0f - cos(omega);
+	b2 = (1.0f - cos(omega)) / 2.0f;
+}
+
+void CMyFilter::HighPass(float freq, float q, float samplerate)
+{
+	float omega = 2.0f * 3.14159265f *  freq / samplerate;
+	float alpha = sin(omega) / (2.0f * q);
+
+	a0 = 1.0f + alpha;
+	a1 = -2.0f * cos(omega);
+	a2 = 1.0f - alpha;
+	b0 = (1.0f + cos(omega)) / 2.0f;
+	b1 = -(1.0f + cos(omega));
+	b2 = (1.0f + cos(omega)) / 2.0f;
+}
+
+void CMyFilter::BandPass(float freq, float bw, float samplerate)
+{
+	float omega = 2.0f * 3.14159265f *  freq / samplerate;
+	float alpha = sin(omega) * sinh(log(2.0f) / 2.0 * bw * omega / sin(omega));
+
+	a0 = 1.0f + alpha;
+	a1 = -2.0f * cos(omega);
+	a2 = 1.0f - alpha;
+	b0 = alpha;
+	b1 = 0.0f;
+	b2 = -alpha;
+}
+
+void CMyFilter::Notch(float freq, float bw, float samplerate)
+{
+	float omega = 2.0f * 3.14159265f *  freq / samplerate;
+	float alpha = sin(omega) * sinh(log(2.0f) / 2.0 * bw * omega / sin(omega));
+
+	a0 = 1.0f + alpha;
+	a1 = -2.0f * cos(omega);
+	a2 = 1.0f - alpha;
+	b0 = 1.0f;
+	b1 = -2.0f * cos(omega);
+	b2 = 1.0f;
+}
+
+void CMyFilter::LowShelf(float freq, float q, float gain, float samplerate)
+{
+	float omega = 2.0f * 3.14159265f *  freq / samplerate;
+	float alpha = sin(omega) / (2.0f * q);
+	float A = pow(10.0f, (gain / 40.0f));
+	float beta = sqrt(A) / q;
+
+	a0 = (A + 1.0f) + (A - 1.0f) * cos(omega) + beta * sin(omega);
+	a1 = -2.0f * ((A - 1.0f) + (A + 1.0f) * cos(omega));
+	a2 = (A + 1.0f) + (A - 1.0f) * cos(omega) - beta * sin(omega);
+	b0 = A * ((A + 1.0f) - (A - 1.0f) * cos(omega) + beta * sin(omega));
+	b1 = 2.0f * A * ((A - 1.0f) - (A + 1.0f) * cos(omega));
+	b2 = A * ((A + 1.0f) - (A - 1.0f) * cos(omega) - beta * sin(omega));
+}
+
+void CMyFilter::HighShelf(float freq, float q, float gain, float samplerate)
+{
+	float omega = 2.0f * 3.14159265f *  freq / samplerate;
+	float alpha = sin(omega) / (2.0f * q);
+	float A = pow(10.0f, (gain / 40.0f));
+	float beta = sqrt(A) / q;
+
+	a0 = (A + 1.0f) - (A - 1.0f) * cos(omega) + beta * sin(omega);
+	a1 = 2.0f * ((A - 1.0f) - (A + 1.0f) * cos(omega));
+	a2 = (A + 1.0f) - (A - 1.0f) * cos(omega) - beta * sin(omega);
+	b0 = A * ((A + 1.0f) + (A - 1.0f) * cos(omega) + beta * sin(omega));
+	b1 = -2.0f * A * ((A - 1.0f) + (A + 1.0f) * cos(omega));
+	b2 = A * ((A + 1.0f) + (A - 1.0f) * cos(omega) - beta * sin(omega));
+}
+
+
+void CMyFilter::Peaking(float freq, float bw, float gain, float samplerate)
+{
+	float omega = 2.0f * 3.14159265f *  freq / samplerate;
+	float alpha = sin(omega) * sinh(log(2.0f) / 2.0 * bw * omega / sin(omega));
+	float A = pow(10.0f, (gain / 40.0f));
+
+	a0 = 1.0f + alpha / A;
+	a1 = -2.0f * cos(omega);
+	a2 = 1.0f - alpha / A;
+	b0 = 1.0f + alpha * A;
+	b1 = -2.0f * cos(omega);
+	b2 = 1.0f - alpha * A;
+}
+
+void CMyFilter::AllPass(float freq, float q, float samplerate)
+{
+	float omega = 2.0f * 3.14159265f *  freq / samplerate;
+	float alpha = sin(omega) / (2.0f * q);
+
+	a0 = 1.0f + alpha;
+	a1 = -2.0f * cos(omega);
+	a2 = 1.0f - alpha;
+	b0 = 1.0f - alpha;
+	b1 = -2.0f * cos(omega);
+	b2 = 1.0f + alpha;
+}
---
+++ Source/PluginEditor.h
@@ -0,0 +1,102 @@
+/*
+  ==============================================================================
+
+  This is an automatically generated GUI class created by the Introjucer!
+
+  Be careful when adding custom code to these files, as only the code within
+  the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
+  and re-saved.
+
+  Created with Introjucer version: 4.1.0
+
+  ------------------------------------------------------------------------------
+
+  The Introjucer is part of the JUCE library - "Jules' Utility Class Extensions"
+  Copyright (c) 2015 - ROLI Ltd.
+
+  ==============================================================================
+*/
+
+#ifndef __JUCE_HEADER_8972AC7388081E28__
+#define __JUCE_HEADER_8972AC7388081E28__
+
+//[Headers]     -- You can add your own extra header files here --
+#include "JuceHeader.h"
+#include "PluginProcessor.h"
+#include "CustomGUI/FilmstripSlider.h"
+#include "CustomGUI/FilmstripToggleButton.h"
+//[/Headers]
+
+
+
+//==============================================================================
+/**
+                                                                    //[Comments]
+    An auto-generated component, created by the Introjucer.
+
+    Describe your class and how it works here!
+                                                                    //[/Comments]
+*/
+class PizzaKnobFilterPluginAudioProcessorEditor  : public AudioProcessorEditor,
+                                                   private Timer,
+                                                   public SliderListener,
+                                                   public ButtonListener,
+                                                   public ComboBoxListener
+{
+public:
+    //==============================================================================
+    PizzaKnobFilterPluginAudioProcessorEditor (PizzaKnobFilterPluginAudioProcessor& p);
+    ~PizzaKnobFilterPluginAudioProcessorEditor();
+
+    //==============================================================================
+    //[UserMethods]     -- You can add your own custom methods in this section.
+    void timerCallback() override;
+	PizzaKnobFilterPluginAudioProcessor* getProcessor() const
+     { return static_cast <PizzaKnobFilterPluginAudioProcessor*>(getAudioProcessor()); }
+    //[/UserMethods]
+
+    void paint (Graphics& g);
+    void resized();
+    void sliderValueChanged (Slider* sliderThatWasMoved);
+    void buttonClicked (Button* buttonThatWasClicked);
+    void comboBoxChanged (ComboBox* comboBoxThatHasChanged);
+
+    // Binary resources:
+    static const char* bg_png;
+    static const int bg_pngSize;
+    static const char* ohtsumami1_png;
+    static const int ohtsumami1_pngSize;
+    static const char* ohtsumami2_png;
+    static const int ohtsumami2_pngSize;
+    static const char* mark_png;
+    static const int mark_pngSize;
+
+
+private:
+    //[UserVariables]   -- You can add your own custom variables in this section.
+    Image image_knob;
+	Image image_toggle;
+    PizzaKnobFilterPluginAudioProcessor& processor;
+	float UserParamRanges[PizzaKnobFilterPluginAudioProcessor::Parameters::totalNumParam];
+    //[/UserVariables]
+
+    //==============================================================================
+    ScopedPointer<FilmstripSlider> Freq;
+    ScopedPointer<FilmstripSlider> Q;
+    ScopedPointer<FilmstripToggleButton> Toggle;
+    ScopedPointer<ComboBox> ModeSelector;
+    ScopedPointer<ImageButton> imageButton;
+    ScopedPointer<ImageButton> imageButton2;
+    ScopedPointer<HyperlinkButton> hyperlinkButton;
+    ScopedPointer<HyperlinkButton> hyperlinkButton2;
+    Image cachedImage_bg_png_1;
+
+
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PizzaKnobFilterPluginAudioProcessorEditor)
+};
+
+//[EndFile] You can add extra defines here...
+//[/EndFile]
+
+#endif   // __JUCE_HEADER_8972AC7388081E28__
